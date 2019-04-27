a4acb93807a5e46fc88a06b225361d43ed870175
andrianm28 <andrianmaulana28@gmail.com>
2019-04-28 01:00:36 +0700

    setup fragments,theme color and some little components

--- grakify/app/src/main/java/com/andrianm28/grakify/MainActivity.java
+++ grakify/app/src/main/java/com/andrianm28/grakify/MainActivity.java
@@ -1,13 +1,98 @@
 package com.andrianm28.grakify;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity {
+import com.andrianm28.grakify.fragment.AccountFragment;
+import com.andrianm28.grakify.fragment.CardFragment;
+import com.andrianm28.grakify.fragment.MapFragment;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        setTitle("GrakVenue");
+        setTitle(Html.fromHtml("<font color='#b22222'>GrakVenue</font>"));
+        centerTitle();
+        // kita set default nya Home Fragment
+        loadFragment(new CardFragment());
+// inisialisasi BottomNavigaionView
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bn_main);
+// beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
     }
+
+    private void centerTitle() {
+        ArrayList<View> textViews = new ArrayList<>();
+
+        getWindow().getDecorView().findViewsWithText(textViews, getTitle(), View.FIND_VIEWS_WITH_TEXT);
+
+        if(textViews.size() > 0) {
+            AppCompatTextView appCompatTextView = null;
+            if(textViews.size() == 1) {
+                appCompatTextView = (AppCompatTextView) textViews.get(0);
+            } else {
+                for(View v : textViews) {
+                    if(v.getParent() instanceof Toolbar) {
+                        appCompatTextView = (AppCompatTextView) v;
+                        break;
+                    }
+                }
+            }
+
+            if(appCompatTextView != null) {
+                ViewGroup.LayoutParams params = appCompatTextView.getLayoutParams();
+                params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                appCompatTextView.setLayoutParams(params);
+                appCompatTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            }
+        }
+    }
+
+    // method untuk load fragment yang sesuai
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fl_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    // method listener untuk logika pemilihan
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.Card:
+                fragment = new CardFragment();
+                break;
+            case R.id.Map:
+                fragment = new MapFragment();
+                break;
+            case R.id.Search:
+                fragment = new AccountFragment();
+                break;
+            case R.id.Account:
+                fragment = new AccountFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
 }
