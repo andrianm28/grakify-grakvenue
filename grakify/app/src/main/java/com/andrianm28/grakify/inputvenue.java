a5cdf39d1beac7f870c2fb0c1d846cf85a446d1d
Muhammad Iqbal <iqbal_roo@yahoo.com>
2019-05-12 16:25:55 +0700

    Input venue activity dan tambahin manives

---
+++ grakify/app/src/main/java/com/andrianm28/grakify/inputvenue.java
@@ -0,0 +1,13 @@
+package com.andrianm28.grakify;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class inputvenue extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inputvenue);
+    }
+}
